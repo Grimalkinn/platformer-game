acae4111626ca2188c0dfa10670e108452a205c2
Grimalkinn <okwuoasfelix634@gmail.com.com>
2024-08-23 14:10:38 +0300

    entities folder failed to push

---
+++ src/entities/Entity.java
@@ -0,0 +1,13 @@
+package entities;
+
+public abstract class Entity {
+
+    protected float xPos, yPos;
+
+    public Entity(int xPos, int yPos){
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+
+    
+}
---
+++ src/entities/Player.java
@@ -0,0 +1,19 @@
+package entities;
+
+
+public class Player extends Entity{
+
+    public Player(int xPos, int yPos) {
+        super(xPos, yPos);
+    }
+
+    public void update() {
+        
+    }
+
+    public void render() {
+        
+    }
+
+    
+}
